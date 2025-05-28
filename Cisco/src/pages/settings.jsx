14bfe9c3a0c55af297ee779b0782accca3368d22
Jonathan0890 <jonathan.coox15@gmail.com>
2025-05-28 18:20:54 -0500

    cambios en setting y profile

--- Cisco/src/pages/settings.jsx
+++ Cisco/src/pages/settings.jsx
@@ -1,44 +1,31 @@
+import { IoIosArrowBack } from "react-icons/io";
 import { NavLink } from "react-router-dom";
 
 export default function Settings() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-sky-400 via-blue-500 to-blue-600">
-      <div className="bg-white rounded-b-3xl px-4 py-6 mb-0">
-        <div className="flex items-center justify-between">
-          <button
-            variant="ghost"
-            size="icon"
-            className="text-sky-500 hover:bg-sky-50"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <NavLink to="/">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 19l-7-7 7-7"
-                />
+    <div className="min-h-screen bg-gradient-to-b from-[#47B4E2] to-[#544197] flex flex-col">
+      <div className="w-full relative">
 
-              </NavLink>
-            </svg>
-          </button>
-          <h1 className="text-xl font-semibold text-sky-500">Configuraciones</h1>
-          <div className="w-10"></div>
+        <div className="bg-white rounded-b-3xl max-w-4xl mx-auto px-6 md:px-20 py-6 ">
+          <div className="flex items-center justify-between">
+            <NavLink to="/">
+              <button>
+                <IoIosArrowBack size={40} />
+              </button>
+            </NavLink>
+            <h1 className="text-3xl font-semibold text-sky-500 mt-8">Configuración</h1>
+            <div className="w-10"></div>
+          </div>
         </div>
       </div>
 
-      <div className="px-6 pt-8">
-        <h2 className="text-white text-md font-semibold mb-4"> Configuraciones</h2>
-        <p className="text-white text-sm leading-relaxed opacity-90">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste praesentium quia eaque,
-          vel corrupti eligendi fugit possimus, ut illum nulla expedita, veritatis deleniti
-          maxime eius voluptates nemo blanditiis voluptatum ullam!
+      <div className="max-w-4xl mx-auto w-full px-6 md:px-20 pt-8 text-left">
+        <h2 className="text-white text-sm font-semibold ">Configuración</h2>
+        <p className="text-white text-sm text-[12px] grid grid-cols-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Iste praesentium quia vel corrupti eligendi fugit possimus,
+          ut illum nulla expedita, veritatis deleniti maxime eius
+          voluptates nemo blanditiis voluptatum ullam!
         </p>
       </div>
     </div>
